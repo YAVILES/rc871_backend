87eb3cbe42f3ff24ec1f3532a215627ada5ea83c
Yonathan Aviles <yonathanaviles123@gmail.com>
2021-10-10 11:37:26 -0400

    Update asgi.py

--- rc871_backend/asgi.py
+++ rc871_backend/asgi.py
@@ -10,7 +10,7 @@ import os
 import django
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'btpb2b.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rc871_backend.settings')
 
 django.setup()
 
