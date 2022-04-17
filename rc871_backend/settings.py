4bd77d07b93101c5f453b3ead0b2adb0d5e97fb3
YAVILES <yonathanaviles123@gmail.com>
2022-04-17 17:15:06 -0400

    Update settings.py

--- rc871_backend/settings.py
+++ rc871_backend/settings.py
@@ -184,8 +184,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL = env('MEDIA_URL')
 
-# MEDIA_ROOT = '/var/www/html/admin871/media/'  # os.path.join(BASE_DIR, "media")
-MEDIA_ROOT = 'C:/xampp/htdocs/admin871/media/'
+MEDIA_ROOT = '/var/www/html/admin871/media/'  # os.path.join(BASE_DIR, "media")
+# MEDIA_ROOT = 'C:/xampp/htdocs/admin871/media/'
 
 AUTH_USER_MODEL = 'security.User'
 AUTHENTICATION_BACKENDS = ['apps.security.backends.CustomAuthenticationBackend', ]
