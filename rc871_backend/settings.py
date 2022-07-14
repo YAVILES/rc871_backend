66869152b31b8e46d415825364e226149bb9981a
YAVILES <yonathanaviles123@gmail.com>
2022-07-14 18:02:08 -0400

    Update settings.py

--- rc871_backend/settings.py
+++ rc871_backend/settings.py
@@ -57,7 +57,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '207.244.233.242']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'api-dev.segurosrc871.com']
 
 # Application definition
 INSTALLED_APPS = [
